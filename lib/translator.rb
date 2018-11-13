d68feb50344b26abc6bf65d5b4829b5fd05de587
Dmitri Samotessov <dimasamo96@gmail.com>
2018-11-13 04:45:47 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,8 +12,9 @@ def load_library(path)
   #binding.pry
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emoticon)
+  hash = load_library(path)
+  hash["get_emoticon"][emoticon]
 end
 
 def get_english_meaning
